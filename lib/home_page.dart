564011247739eb6adc2900d330fa84337125112e
nujal <nujal.bct74043@kecktm.edu.np>
2022-06-01 16:51:42 +0545

    update regarding btmnavigational

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,10 @@
-import 'package:class_list/job_opening.dart';
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:class_list/pages/1st.dart';
+import 'package:class_list/pages/2nd.dart';
+import 'package:class_list/pages/3rd.dart';
+import 'package:class_list/pages/4th.dart';
+import 'package:class_list/pages/job_opening.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,64 +17,92 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
-
+  PageController _pageController = new PageController();
   final List<Widget> _pages = const [
-    Center(
-      child: Text("Home"),
-    ),
-    Center(
-      child: Text("Search"),
-    ),
+    FirstPage(),
+    SecondPage(),
+    ThirdPage(),
+    FourthPage(),
     JobOpening(),
-    Center(
-      child: Text("test"),
-    ),
-    Center(
-      child: Text("test 2"),
-    ),
+  ];
+
+  final items = <Widget>[
+    Icon(Icons.home),
+    Icon(Icons.search),
+    Icon(Icons.favorite),
+    Icon(Icons.settings),
+    Icon(Icons.person),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber,
+      extendBody: true,
       // appBar: AppBar(
       //   title: Text('Yaj Tech'),
       //   centerTitle: true,
       // ),
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        // backgroundColor: Colors.grey,
-        selectedIconTheme: IconThemeData(color: Colors.blue),
-        unselectedIconTheme: IconThemeData(color: Colors.grey),
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedLabelStyle: TextStyle(
-          color: Colors.blue,
-        ),
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        unselectedLabelStyle: TextStyle(color: Colors.grey),
-        currentIndex: _currentIndex,
-        onTap: (index) {
+
+      // body: _pages[_currentIndex],
+      body: PageView(
+        controller: _pageController,
+        children: _pages,
+        onPageChanged: (index) {
           setState(() {
             _currentIndex = index;
           });
         },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: "Search",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag),
-            label: "Job Opening",
-          ),
-        ],
       ),
+      bottomNavigationBar: CurvedNavigationBar(
+        animationCurve: Curves.easeOut,
+        animationDuration: const Duration(milliseconds: 400),
+        height: 70,
+        index: _currentIndex,
+        items: items,
+        backgroundColor: Colors.transparent,
+        color: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          _pageController.jumpToPage(_currentIndex);
+        },
+      ),
+      // _pages[_currentIndex],
+      // bottomNavigationBar: BottomNavigationBar(
+      //   // backgroundColor: Colors.grey,
+      //   selectedIconTheme: IconThemeData(color: Colors.blue),
+      //   unselectedIconTheme: IconThemeData(color: Colors.grey),
+      //   showSelectedLabels: true,
+      //   showUnselectedLabels: true,
+      //   selectedLabelStyle: TextStyle(
+      //     color: Colors.blue,
+      //   ),
+      //   selectedItemColor: Colors.blue,
+      //   unselectedItemColor: Colors.grey,
+      //   unselectedLabelStyle: TextStyle(color: Colors.grey),
+      //   currentIndex: _currentIndex,
+      //   onTap: (index) {
+      //     setState(() {
+      //       _currentIndex = index;
+      //     });
+      //   },
+      //   items: const [
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home),
+      //       label: "Home",
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.search),
+      //       label: "Search",
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.shopping_bag),
+      //       label: "Job Opening",
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
